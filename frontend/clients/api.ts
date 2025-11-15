34bea43725235dfb629bb7cd34a06170418b9ce4
Krushna03 <krushnasakhare965@gmail.com>
2025-11-15 13:45:25 +0530

    chore: update GraphQL API endpoint to use environment variable for flexibility

--- frontend/clients/api.ts
+++ frontend/clients/api.ts
@@ -2,7 +2,10 @@ import { GraphQLClient } from "graphql-request";
 
 const isCLient = typeof window !== "undefined";
 
-export const graphqlClient = new GraphQLClient("http://localhost:8000/graphql", 
+const baseURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+const API_URL = baseURL.endsWith("/graphql") ? baseURL : `${baseURL}/graphql`;
+
+export const graphqlClient = new GraphQLClient(API_URL, 
   {
     headers: () => ({
       Authorization: isCLient ? `Bearer ${window.localStorage.getItem("_twitter_token")}` : ""
